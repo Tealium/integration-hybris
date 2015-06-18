6c3126f9600092a150a1102dc77b2d40cca4b5b2
patrickmcwilliams <patrick.mcwilliams@tealium.com>
2015-06-18 13:31:47 -0700

    fix package name

---
+++ hybris/bin/custom/tealiumiqaddon/acceleratoraddon/web/src/com/tealium/dataconnector/hybris/TealiumCustomData.java
@@ -0,0 +1,7 @@
+package com.tealium.dataconnector.hybris;
+
+import com.tealium.dataconnector.hybris.TealiumCustomDataController;
+
+public class TealiumCustomData extends TealiumCustomDataController {
+
+}
