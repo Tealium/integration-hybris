58c3959ef864a3ed09d6c93d69b6e0cbf2f82db4
patrickmcwilliams <patrick.mcwilliams@tealium.com>
2015-06-18 22:19:43 -0700

    reverted to no custom data

--- hybris/bin/custom/tealiumiqaddon/acceleratoraddon/web/src/com/tealium/dataconnector/hybris/TealiumCustomData.java
+++
@@ -1,7 +0,0 @@
-package com.tealium.dataconnector.hybris;
-
-import com.tealium.dataconnector.hybris.TealiumCustomDataController;
-
-public class TealiumCustomData extends TealiumCustomDataController {
-
-}
